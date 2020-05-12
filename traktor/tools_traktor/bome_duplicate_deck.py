b26eba9ed97b3882c6592c88b666012ed68b3c52
Pedro Estrela <pedro.estrela@gmail.com>
2020-05-12 20:18:45 +0100

    fast upload

--- traktor/tools_traktor/bome_duplicate_deck.py
+++ traktor/tools_traktor/bome_duplicate_deck.py
@@ -48,7 +48,7 @@ def do_var_replace(data, v_in, v_out, *, oper, where="front"):
   return data
 
 
-def do_variables_inner(line, step):
+def change_variables(line, step):
   line = RString(line)
   
   if not line.re_search(r"^Option"):
@@ -82,26 +82,59 @@ def do_variables_inner(line, step):
       
   return line
   
+
+def change_channel(line, step):
+  line = RString(line)
   
-def do_variables(ml, step):
+  ch_in='Channel num="0"'
+  ch_out='Channel num="%d"' % (step)
+  
+  line = line.replace(ch_in, ch_out)
+  return line
+
+    
+def change_timer(line, step):
+  line = RString(line)
+  
+  if not (line.re_search(r"^Incoming") or line.re_search(r"^Outgoing")):
+    return line
+  
+  if step == 1:
+    printd(opts.debug, line)
+    
+    # https://docs.python.org/3/library/re.html#re.sub
+    
+    # r"\g<1>2"
+    out_expression = r'\g<1>%d' % (step+1)
+    line = line.re_sub(r'(_.*?)1', out_expression)
+
+    printd(opts.debug, line)
+    #sys.exit(1)
+
+  return line  
+  
+   
+def manipulate_st(st, step):
+  if opts.do_variables:
+    st = change_variables(st, step)
+  
+  if opts.do_timers:
+    st = change_timer(st, step)
+  
+  if opts.do_channels:
+    st = change_channel(st, step)
+
+  return st
+  
+  
+  
+def manipulate_ml(ml, step):
   lines = ml_to_st(ml, clean=False)
-  lines = [do_variables_inner(line, step) for line in lines ]
+  lines = [manipulate_st(line, step) for line in lines ]
   ml = st_to_ml(lines)
   return ml
   
  
-    
-def do_timers(data):
-  """
-  https://regex101.com/
-   in: (_.*)1
-   out: \12
-   flags: gmU
-  
-  """
-  for line in data.splitlines():
-    if "Incoming" in line:
-      print(line)
 
       
 
@@ -119,8 +152,37 @@ parser.add_argument('-D', '--fast_debug', dest="fast_debug", default=False, acti
 parser.add_argument('-s', '--step', dest="step", default=1,type=int,
                     help='step')
                                   
+
+parser.add_argument('-v', '--variables', dest="only_variables", default=False, action="store_true",
+                    help='only do variables')
+parser.add_argument('-t', '--timers', dest="only_timers", default=False, action="store_true",
+                    help='only do variables')
+parser.add_argument('-c', '--channels', dest="only_channels", default=False, action="store_true",
+                    help='only do variables')
+
                                   
 opts = parser.parse_args()
+
+
+opts.do_variables = True
+opts.do_timers = True
+opts.do_channels = True
+
+if opts.only_variables:
+  opts.do_variables = True
+  opts.do_timers = False
+  opts.do_channels = False
+  
+if opts.only_timers:
+  opts.do_variables = False
+  opts.do_timers = True
+  opts.do_channels = False
+  
+if opts.only_channels:
+  opts.do_variables = False
+  opts.do_timers = False
+  opts.do_channels = True
+
 
 
 opts.file_out= DPath(opts.file_in).with_suffix(".out.bmtp")
@@ -132,12 +194,12 @@ if opts.fast_debug:
 
      
 with open(opts.file_in, "r") as fd:
-  data = fd.read()
+  ml = fd.read()
   
-data = do_variables(data, opts.step)
+ml = manipulate_ml(ml, opts.step)
   
 with open(opts.file_out, "w") as fd:
-  fd.write(data)
+  fd.write(ml)
   
 print("Wrote: %s." % (opts.file_out))
 
